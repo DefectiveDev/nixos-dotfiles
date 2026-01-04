859df181616c9436f83b5a6e9e6a66ae789022e3
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-04 18:15:31 -0500

    generic auto closing enabled with autopairs now nvim

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,5 +1,32 @@
 return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = { enable_moveright = false, map_bs = false, enable_afterquote= false, enable_bracket_in_quote = false }
+    config = function ()
+        local npairs = require'nvim-autopairs'
+        local Rule = require'nvim-autopairs.rule'
+        local cond = require 'nvim-autopairs.conds'
+
+        npairs.setup {
+            enable_moveright = false,
+            map_bs = false,
+            enable_afterquote= false,
+            enable_bracket_in_quote = false
+        }
+
+        npairs.add_rule(Rule('<', '>', {
+            -- if you use nvim-ts-autotag, you may want to exclude these filetypes from this rule
+            -- so that it doesn't conflict with nvim-ts-autotag
+            '-html',
+            '-javascriptreact',
+            '-typescriptreact',
+        }):with_pair(
+            -- regex will make it so that it will auto-pair on
+            -- `a<` but not `a <`
+            -- The `:?:?` part makes it also
+            -- work on Rust generics like `some_func::<T>()`
+            cond.before_regex('%a+:?:?$', 3)
+        ):with_move(function(opts)
+            return opts.char == '>'
+        end))
+    end
 }
