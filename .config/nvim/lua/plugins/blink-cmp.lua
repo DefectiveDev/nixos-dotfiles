d90ba049b2088f466c852659e805a1c053ef2c42
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-07 07:27:39 -0500

    blink-cmp set with old icons, similar keeymap to cmp, removed scroll bar, added transparent background

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -3,6 +3,7 @@ return {
     -- optional: provides snippets for the snippet source
     dependencies = { 'rafamadriz/friendly-snippets' },
 
+    event = "LspAttach",
     -- use a release tag to download pre-built binaries
     version = '1.*',
     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
@@ -25,13 +26,32 @@ return {
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'enter' },
+        keymap = {
+            preset = 'none',
+
+            ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            ['<C-e>'] = { 'hide', 'fallback' },
+            ['<CR>'] = { 'accept', 'fallback' },
+
+            ['<Tab>'] = { 'snippet_forward', 'fallback' },
+            ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+            ['<Up>'] = { 'select_prev', 'fallback' },
+            ['<Down>'] = { 'select_next', 'fallback' },
+            ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
+            ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
+
+            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+            -- ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+        },
 
         appearance = {
             -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
             -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = 'normal',
-            kind_icon = {
+            kind_icons = {
                 Text = "󰉿",
                 Method = "󰆧",
                 Function = "󰊕",
@@ -61,7 +81,16 @@ return {
         },
 
         -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = true } },
+        completion = {
+            menu = {
+                scrollbar = false,
+                scrolloff = 2,
+            },
+
+            documentation = {
+                auto_show = true
+            }
+        },
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
