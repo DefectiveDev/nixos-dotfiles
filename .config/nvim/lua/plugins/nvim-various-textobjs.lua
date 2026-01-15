5d8691fb5698ec2b8833cc8ac9cf8510e306ff6a
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-15 09:04:31 -0500

    NVIM Feature: nvim-various-textobjs now only uses specified keymappings

--- .config/nvim/lua/plugins/nvim-various-textobjs.lua
+++ .config/nvim/lua/plugins/nvim-various-textobjs.lua
@@ -1,9 +1,53 @@
 return {
 	"chrisgrieser/nvim-various-textobjs",
-	event = "VeryLazy",
+    keys = {
+        { "ii",
+            function ()
+                require("various-textobjs").indentation("inner", "inner")
+            end,
+            mode = {"o", "x"},
+            desc = "inner-inner indentation textobj"
+        },
+        { "ai",
+            function ()
+                require("various-textobjs").indentation("outer", "inner")
+            end,
+            mode = {"o", "x"},
+            desc = "outer-inner indentation textobj"
+        },
+        { "iI",
+            function ()
+                require("various-textobjs").indentation("inner", "inner")
+            end,
+            mode = {"o", "x"},
+            desc = "inner-inner indentation textobj"
+        },
+        { "aI",
+            function ()
+                require("various-textobjs").indentation("outer", "outer")
+            end,
+            mode = {"o", "x"},
+            desc = "outer-outer indentation textobj"
+        },
+        { "isw",
+            function ()
+                require("various-textobjs").subword("inner")
+            end,
+            mode = {"o", "x"},
+            desc = "inner subword textobj"
+        },
+        { "asw",
+            function ()
+                require("various-textobjs").subword("outer")
+            end,
+            mode = {"o", "x"},
+            desc = "outer subword textobj"
+        }
+
+    },
 	opts = {
 		keymaps = {
-			useDefaults = true
+			useDefaults = false
 		}
 	},
 }
