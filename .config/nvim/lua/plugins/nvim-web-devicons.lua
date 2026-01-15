e07064e2971d3bc4c3246ca9a4c9d44eecf738c1
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-15 14:46:23 -0500

    NVIM Refactor: Removed unnecessary dependancy for devicons

--- .config/nvim/lua/plugins/nvim-web-devicons.lua
+++ .config/nvim/lua/plugins/nvim-web-devicons.lua
@@ -1,8 +1,5 @@
 return {
 	"nvim-tree/nvim-web-devicons",
 	lazy = true,
-	dependencies = { "NotNullBool/nvim-material-icon" },
-	opts = function()
-		return { override = require("nvim-material-icon").get_icons() }
-	end,
+	opts = {},
 }
