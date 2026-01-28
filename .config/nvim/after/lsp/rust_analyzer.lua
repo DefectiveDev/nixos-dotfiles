bbad3089aaead68713e4f0b3fa249156dcf72dd7
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-27 23:17:53 -0500

    NVIM Feature: Rust analyzer disable paraenthesis, semicolons, and argument snippets from completion

--- .config/nvim/after/lsp/rust_analyzer.lua
+++ .config/nvim/after/lsp/rust_analyzer.lua
@@ -1,3 +1,40 @@
 return {
-
+    settings = {
+        ["rust-analyzer"] = {
+            completion = {
+                addSemicolonToUnit = false,
+                addCallParenthesis = false,
+                addCallArgumentSnippets = false
+            },
+            lens = {
+                debug = {
+                    enable = true
+                },
+                enable = true,
+                implementations = {
+                    enable = true
+                },
+                references = {
+                    adt = {
+                        enable = true
+                    },
+                    enumVariant = {
+                        enable = true
+                    },
+                    method = {
+                        enable = true
+                    },
+                    trait = {
+                        enable = true
+                    }
+                },
+                run = {
+                    enable = true
+                },
+                updateTest = {
+                    enable = true
+                }
+            }
+        }
+    }
 }
