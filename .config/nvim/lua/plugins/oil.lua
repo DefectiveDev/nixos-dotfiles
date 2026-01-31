e98951db5117f1806cc446f1cde1a6448c839b54
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-31 16:28:06 -0500

    nvim feat(oil.nvim) now shows parent directory by default

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -6,7 +6,13 @@ return {
     keys = {{
         "-", "<CMD>Oil<CR>", desc= "Open parent directory (Oil)"
     }},
-    opts = {},
+    opts = {
+        view_options = {
+            is_hidden_file = function(name, bufnr)
+                return name ~= ".." and vim.startswith(name, ".")
+            end,
+        }
+    },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons", },
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
