cf0b1107fcccdfe0938f9d372ec91f0afff052f8
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-31 12:34:19 -0500

    nvim feat(aerial): disabled filter for document symbols

--- .config/nvim/lua/plugins/telescope_nvim/core.lua
+++ .config/nvim/lua/plugins/telescope_nvim/core.lua
@@ -109,6 +109,7 @@ return {{
     keys = {{"<leader>fd", "<cmd>Telescope aerial<cr>", desc = "Find document symbol in buffer (Aerial-Telescope)" }},
     dependencies = { "nvim-telescope/telescope.nvim" },
     opts = {
+        filter_kind = false,
         backends = { "lsp","treesitter", "markdown", "asciidoc", "man" },
     },
     config = function (_, opts)
