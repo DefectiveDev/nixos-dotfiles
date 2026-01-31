017f67d50cb05e659b4865a284d734adf9b2f8c7
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-31 11:58:05 -0500

    nvim fix(lazydev) reverted and removed cmd lazyload
    cmd lazy load was causing this not to load before lsp. This is what was
    causing errors.

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,7 +1,7 @@
 return {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets', "folke/lazydev.nvim" },
+    dependencies = { 'rafamadriz/friendly-snippets', --[[ "folke/lazydev.nvim"  ]]},
 
     event = {  "LspAttach", "InsertEnter", "CmdlineEnter" },
     -- use a release tag to download pre-built binaries
@@ -108,18 +108,6 @@ return {
         -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
             default = {'lsp', 'path', 'snippets', 'buffer' },
-
-            per_filetype = {
-                lua = { inherit_defaults = true, "lazydev" }
-            },
-            providers = {
-                lazydev = {
-                    name = "LazyDev",
-                    module = "lazydev.integrations.blink",
-                    -- make lazydev completions top priority (see `:h blink.cmp`)
-                    score_offset = 100,
-                },
-            },
         },
 
         -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
