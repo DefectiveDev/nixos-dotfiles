ecb1155b788bf0bdc90561fead973f5077762d21
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-01 12:30:07 -0500

    nvim refactor(keymaps) changed descriptions to show obious changes and naming convention

--- .config/nvim/lua/core/key-map.lua
+++ .config/nvim/lua/core/key-map.lua
@@ -4,8 +4,8 @@ keymap.set({"n", "x", "o"}, "S", "<Nop>", {nowait = true})
 
 keymap.set({"n", "x", "o"}, "<leader>`", "~")
 
-keymap.set( "n", "<leader>o", ':<C-u>call append(line("."),   repeat([""], v:count1))<CR>',  {silent = true, desc = "Add line above"})
-keymap.set( "n", "<leader>O", ':<C-u>call append(line(".")-1, repeat([""], v:count1))<CR>',  {silent = true, desc = "Add line below"})
+keymap.set( "n", "<leader>o", ':<C-u>call append(line("."),   repeat([""], v:count1))<CR>',  {silent = true, desc = "[o]ppend line above (nvim)"})
+keymap.set( "n", "<leader>O", ':<C-u>call append(line(".")-1, repeat([""], v:count1))<CR>',  {silent = true, desc = "[O]ppend line below (nvim)"})
 
 keymap.set('c', '<C-H>', '<left>')
 keymap.set('c', '<C-L>', '<right>')
@@ -15,40 +15,40 @@ keymap.set('i', "<C-H>", "<left>")
 keymap.set('i', "<C-J>", "<down>")
 keymap.set('i', "<C-K>", "<up>")
 
-keymap.set('n', "<localleader>ff", "gg=G''", {desc="format file", silent =true})
+keymap.set('n', "<localleader>ff", "gg=G''", {desc="[f]ormat [f]ile (nvim)", silent =true})
 
-keymap.set('n', "J", "mzJ`z", {desc = "Append next line", silent = true})
+keymap.set('n', "J", "mzJ`z", {desc = "[J]oin next line (nvim)", silent = true})
 
 keymap.set('n', 'n', "nzzzv", {silent = true})
 keymap.set('n', 'N', "Nzzzv", {silent = true})
 
-keymap.set('n', '<C-j>', ":m .+1<CR> ==",{ desc = "Line down", silent = true})
-keymap.set('n', '<C-k>', ":m .-2<CR> ==",{ desc = "Line up", silent = true})
+keymap.set('n', '<C-j>', ":m .+1<CR> ==",{ desc = "Move line down (nvim)", silent = true})
+keymap.set('n', '<C-k>', ":m .-2<CR> ==",{ desc = "Move line up (nvim)", silent = true})
 
-keymap.set('x', '<C-k>', ":m '<-2<CR>gv=gv",{ desc = "Line up", silent = true})
-keymap.set('x', '<C-j>', ":m '>+1<CR>gv=gv",{ desc= "Line down", silent = true})
+keymap.set('x', '<C-k>', ":m '<-2<CR>gv=gv",{ desc = "Move line up (nvim)", silent = true})
+keymap.set('x', '<C-j>', ":m '>+1<CR>gv=gv",{ desc= "Move line down (nvim)", silent = true})
 
-keymap.set({'v','n'}, "<leader>r", "r",{ desc = "Regular replace"})
-keymap.set({'v','n'}, "<leader>x", "x",{ desc = "Regular char delete"})
-keymap.set({'v','n'}, "r", "\"_r",{ desc = "Void replace"})
-keymap.set({'v','n'}, "x", "\"_x",{ desc = "Void char delete"})
-keymap.set({'v','n'}, "<leader>d", "\"_d",{ desc = "Void delete", noremap = true})
-keymap.set({'v','n'}, '<leader>c', "\"_c", {desc = "Void change", noremap = false})
-keymap.set({'v','n'}, '<leader>C', "\"_C", {desc = "Void change", noremap = false})
+keymap.set({'v','n'}, "<leader>r", "r",{ desc = "Regular [r]eplace (nvim)"})
+keymap.set({'v','n'}, "<leader>x", "x",{ desc = "Regular char delete (nvim)"})
+keymap.set({'v','n'}, "r", "\"_r",{ desc = "Void [r]eplace (nvim)"})
+keymap.set({'v','n'}, "x", "\"_x",{ desc = "Void char delete (nvim)"})
+keymap.set({'v','n'}, "<leader>d", "\"_d",{ desc = "Void [d]elete (nvim)", noremap = true})
+keymap.set({'v','n'}, '<leader>c', "\"_c", {desc = "Void [c]hange (nvim)", noremap = false})
+keymap.set({'v','n'}, '<leader>C', "\"_C", {desc = "Void [C]hange (nvim)", noremap = false})
 
-keymap.set({'v','n'}, "<leader>y", "\"+y",{ desc = "Yank to clipboard" })
+keymap.set({'v','n'}, "<leader>y", "\"+y",{ desc = "[y]ank to clipboard (nvim)" })
 
-keymap.set({'v','n'}, "<leader>p", "\"+p", {desc = "Paste from clipboard"})
-keymap.set({'v','n'}, "<leader>P", "\"+P", {desc = "Paste from clipboard"})
-keymap.set('x', "<leader>p", "\"_dh\"+p", {desc = "Paste from clipboard void selection"})
-keymap.set('x', "<leader>P", "\"+p", {desc = "Paste from clipboard copy selection"})
-keymap.set('x', 'p', "\"_dP", {desc= "Paste and void selection"})
-keymap.set('x', 'P', "p", {desc= "Paste and copy selection"})
+keymap.set({'v','n'}, "<leader>p", "\"+p", {desc = "[p]aste from clipboard after cursor (nvim)"})
+keymap.set({'v','n'}, "<leader>P", "\"+P", {desc = "[P]aste from clipboard before cursor (nvim)"})
+keymap.set('x', "<leader>p", "\"_dh\"+p", {desc = "[p]aste from clipboard void selection (nvim)"})
+keymap.set('x', "<leader>P", "\"+p", {desc = "[P]aste from clipboard copy selection (nvim)"})
+keymap.set('x', 'p', "\"_dP", {desc= "[p]aste and void selection (nvim)"})
+keymap.set('x', 'P', "p", {desc= "[P]aste and copy selection (nvim)"})
 
-keymap.set('n', "<leader>k", ":cprev<CR>zz", {desc = "Quick fix select previous"})
-keymap.set('n', "<leader>j", ":cnext<CR>zz", {desc = "Quick fix select next"})
+keymap.set('n', "<leader>k", ":cprev<CR>zz", {desc = "Quick fix select previous (nvim)"})
+keymap.set('n', "<leader>j", ":cnext<CR>zz", {desc = "Quick fix select next (nvim)"})
 
-keymap.set('n', "<leader>q", ":cclose<CR>", {desc = "Close quick fix"})
+keymap.set('n', "<leader>q", ":cclose<CR>", {desc = "[q]uick close quick fix (nvim)"})
 
 keymap.set('', ';', ':')
 keymap.set('', ':', ';')
@@ -65,7 +65,7 @@ keymap.set('n', '0', ":silent! call search('^.')<CR>^", {silent = true})
 keymap.set('n', '<leader>lz', function ()
     vim.opt.lazyredraw = not vim.opt.lazyredraw:get()
     print(string.format("Lazy redraw is: %s", vim.opt.lazyredraw:get()))
-end, {desc= "Toggle lazy redraw."})
+end, {desc= "Toggle [l]a[z]y redraw. (nvim)"})
 
 -- Allow me to tab over specified chars.
 -- keymap.set('i', "<TAB>",function ()
