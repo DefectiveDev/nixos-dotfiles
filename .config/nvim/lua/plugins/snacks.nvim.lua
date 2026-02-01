0dbf1952b67bef0fc366f718e9609cf2551f97da
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-01 09:43:45 -0500

    nvim refactor(snacks.nvim) use Snacks lua function for lazy git

--- .config/nvim/lua/plugins/snacks.nvim.lua
+++ .config/nvim/lua/plugins/snacks.nvim.lua
@@ -1,8 +1,6 @@
 return {
     "folke/snacks.nvim",
     ---@type snacks.Config
-    keys = { {"<leader>lg", function () require("snacks").lazygit.open() end} },
-    opts = {
-        lazygit = { enabled = true },
-    },
+    keys = { {"<leader>lg", function () Snacks.lazygit.open() end, desc = "Open LazyGit (Snacks)"} },
+    opts = { lazygit = { enabled = true } },
 }
