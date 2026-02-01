71c0ef7e469750e34a2e957ade111a01c9e62cd4
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-01 12:40:47 -0500

    nvim feat(lazygit snacks) now feels whole screen

--- .config/nvim/lua/plugins/snacks.nvim-lazygit.lua
+++ .config/nvim/lua/plugins/snacks.nvim-lazygit.lua
@@ -2,5 +2,13 @@ return {
     "folke/snacks.nvim",
     ---@type snacks.Config
     keys = { {"<leader>lg", function () Snacks.lazygit.open() end, desc = "Open [L]azy[G]it (Snacks)"} },
-    opts = { lazygit = { enabled = true } },
+    opts = {
+        lazygit = {
+            enabled = true,
+            win = {
+                height = 0.99,
+                width = 0.99
+            }
+        },
+    },
 }
