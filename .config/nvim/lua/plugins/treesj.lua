50083ed4f9f16123ce57c21aacf88ef3e567dfd7
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-04 22:16:59 -0500

    nvim refactor(treesj) opted to move plugin comment to end of description like other keymaps

--- .config/nvim/lua/plugins/treesj.lua
+++ .config/nvim/lua/plugins/treesj.lua
@@ -1,7 +1,7 @@
 return {
     'https://github.com/Wansmer/treesj.git',
 	pin = true,
-    keys = { { "<leader>m", function() require("treesj").toggle() end, desc = "Toggle (TSJ) [m]ember block" }, },
+    keys = { { "<leader>m", function() require("treesj").toggle() end, desc = "Toggle [m]ember block (treesj)" }, },
     cmd = {"TSJToggle", "TSJSplit", "TSJJoin"},
     dependencies = { 'https://github.com/nvim-treesitter/nvim-treesitter.git' },
     opts = { use_default_keymaps = false, max_join_length = 9999 }
