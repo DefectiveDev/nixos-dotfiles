b1a02ed54d8331bc62f78c6c5f938df58b66e89c
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-04 22:15:54 -0500

    nvim refactor(grapple) decouppled telescope from grapple

--- .config/nvim/lua/plugins/telescope_nvim/grapple.nvim.lua
+++ .config/nvim/lua/plugins/telescope_nvim/grapple.nvim.lua
@@ -1,13 +1,15 @@
 return {
     "https://github.com/DefectiveDev/grapple.nvim.git",
     pin = true,
-    dependencies = {  "https://github.com/nvim-tree/nvim-web-devicons.git", "https://github.com/nvim-telescope/telescope.nvim.git",},
-    keys = {
-        { "<leader>gt", ":Grapple toggle<cr>", desc = "[g]rapple [t]oggle add/remove a file (Grapple)", silent = true },
-        { "<leader>gg", ":Telescope grapple tags<cr>", desc = "[g]et [g]rapple tags (Grapple + Telescope)", silent = true}
-    },
-    opts = {
-        scope = "git",
-        icons = true,
-    },
+    dependencies = {  "https://github.com/nvim-tree/nvim-web-devicons.git", },
+    keys = { { "<leader>gt", ":Grapple toggle<cr>", desc = "[g]rapple [t]oggle add/remove a file (Grapple)", silent = true } },
+    opts = { scope = "git", icons = true },
+    specs = {
+        "https://github.com/nvim-telescope/telescope.nvim.git",
+        cmd = {"Telescope"},
+        specs = {{
+            "https://github.com/DefectiveDev/grapple.nvim.git",
+            keys = { { "<leader>gg", ":Telescope grapple tags<cr>", desc = "[g]et [g]rapple tags (Grapple + Telescope)", silent = true } }
+        }}
+    }
 }
