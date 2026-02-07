f020cef5dbf4eb87f69b11c994c4425aaf80af01
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-07 00:21:36 -0500

    nvim refactor(project) will no longer load telescope with project nvim.

--- .config/nvim/lua/plugins/telescope_nvim/project.nvim.lua
+++ .config/nvim/lua/plugins/telescope_nvim/project.nvim.lua
@@ -1,7 +1,12 @@
 return {
     "https://github.com/DrKJeff16/project.nvim.git",
     pin = true,
-    specs = { "https://github.com/nvim-telescope/telescope.nvim.git" },
+    specs = {
+        "https://github.com/nvim-telescope/telescope.nvim.git",
+        config = function ()
+            require("telescope").load_extension("projects")
+        end
+    },
     event = {"BufReadPre", "BufNewFile"},
     keys = {
         {"<leader>fp",function ()
@@ -17,7 +22,6 @@ return {
     opts = { patterns = { ".envrc", ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" } },
     config = function(_, opts)
         require("project").setup(opts)
-        require("telescope").load_extension("projects")
         function _ADD_CURR_DIR_TO_PROJECTS()
             local historyfile = require("project_nvim.utils.path").historyfile
             local curr_directory = vim.fn.expand( "%:p:h" )
