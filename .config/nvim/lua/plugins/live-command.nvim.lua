9de1937677c1bdf34a6d19d1191b3831453e0409
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-07 09:29:52 -0500

    nvim feat(live-command) disabled command abbrevations

--- .config/nvim/lua/plugins/live-command.nvim.lua
+++ .config/nvim/lua/plugins/live-command.nvim.lua
@@ -6,10 +6,10 @@ return {
         "Norm",
         "Exe"
     },
-    init = function ()
-        vim.cmd("cnoreabbrev norm norm!")
-        vim.cmd("cnoreabbrev exe Exe")
-    end,
+    -- init = function ()
+    --     vim.cmd("cnoreabbrev norm norm!")
+    --     vim.cmd("cnoreabbrev exe Exe")
+    -- end,
     keys = {
         {"<leader>;",  ":Exe \"\"<left>", mode = "n", desc = "Shortcut for [;]Exe"},
         {"<leader>;",  ":<home>Exe \"<end>norm! \"<left>", mode = "x", desc = "Shortcut for [;]Exe norm! with selection"}
