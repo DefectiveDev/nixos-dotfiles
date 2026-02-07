8debee2ecb9cbd8033b70ddfb74e30256250d665
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-07 02:18:00 -0500

    nvim fix(rainbow-delimiters) no longer has white parenthesis

--- .config/nvim/lua/plugins/rainbow-delimiters.nvim.lua
+++ .config/nvim/lua/plugins/rainbow-delimiters.nvim.lua
@@ -4,7 +4,6 @@ return {
     dependencies = "https://github.com/nvim-treesitter/nvim-treesitter.git",
     lazy = true,
     main = "rainbow-delimiters.setup",
-    -- BUG: first demlimiter is always white
     opts = {
         strategy = {
             [''] = 'rainbow-delimiters.strategy.global',
@@ -28,6 +27,17 @@ return {
         },
         -- blacklist = {'c', 'cpp'},
     },
+    config = function (_, opts)
+        local colors = require("dracula").colors()
+		vim.api.nvim_set_hl(0, "RainbowDelimiterRed", {fg = colors.red})
+		vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", {fg = colors.yellow})
+		vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", {fg = colors.bright_blue})
+		vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", {fg = colors.orange})
+		vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", {fg = colors.green})
+		vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", {fg = colors.purple})
+		vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", {fg = colors.cyan})
+        require("rainbow-delimiters.setup").setup(opts)
+    end,
     specs = {{
         "https://github.com/nvim-treesitter/nvim-treesitter.git",
         config = function ()
