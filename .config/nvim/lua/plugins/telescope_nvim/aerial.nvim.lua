0f962d10ff3a212761fd2421ebaa7cbb6bd78233
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-07 14:26:55 -0500

    nvim refactor(aerial) aerial will no longer force telescope to load

--- .config/nvim/lua/plugins/telescope_nvim/aerial.nvim.lua
+++ .config/nvim/lua/plugins/telescope_nvim/aerial.nvim.lua
@@ -2,13 +2,17 @@ return {
     "https://github.com/stevearc/aerial.nvim.git",
     pin = true,
     keys = {{"<leader>gs", "<cmd>Telescope aerial<cr>", desc = "[g]o to document [s]ymbol in buffer (Aerial + Telescope)" }},
-    dependencies = { "https://github.com/nvim-telescope/telescope.nvim.git" },
     opts = {
         filter_kind = false,
         backends = { "lsp","treesitter", "markdown", "asciidoc", "man" },
     },
     config = function (_, opts)
         require("aerial").setup(opts)
-        require("telescope").load_extension("aerial")
-    end
+    end,
+    specs = {
+        "https://github.com/nvim-telescope/telescope.nvim.git",
+        config = function ()
+            require("telescope").load_extension("aerial")
+        end
+    }
 }
