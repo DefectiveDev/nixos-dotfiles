0a0801a1259cd82fdd665a8e0f9e3ccd797667c1
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-07 15:24:42 -0500

    nvim (colorizer) now shows documentation with color

--- .config/nvim/lua/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/nvim-colorizer.lua
@@ -6,9 +6,25 @@ return {
 	    filetypes = {
 	        -- include everything else
             "*",
+            noice = {
+                rgb_fn = true,
+                always_update = true,
+            },
+            [ "blink-cmp-menu" ] = {
+                rgb_fn = true,
+                always_update = true,
+            },
+            ["blink-cmp-signature"] = {
+                rgb_fn = true,
+                always_update = true,
+            },
+            ["blink-cmp-documentation"] = {
+                rgb_fn = true,
+                always_update = true,
+            },
             -- exclude these filetypes
             "!TelescopePrompt",
             "!lazy"
-        }
+        },
 	},
 }
