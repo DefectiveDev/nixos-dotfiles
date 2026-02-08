f10c3cea985f144516bd140b6a4b596a5bfcb471
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-08 15:56:30 -0500

    nvim refactor(orgmode) moved settings to opts.

--- .config/nvim/lua/plugins/lsp/orgmode.lua
+++ .config/nvim/lua/plugins/lsp/orgmode.lua
@@ -1,12 +1,14 @@
 return {
     "https://github.com/nvim-orgmode/orgmode",
+    pin = true,
     event = "VeryLazy",
-    config = function()
+    opts = {
+        org_agenda_files = "~/Documents/orgfiles/**/*",
+        org_default_notes_file = "~/Documents/orgfiles/refile.org",
+    },
+    config = function(_, opts)
         -- Setup orgmode
-        require("orgmode").setup({
-            org_agenda_files = "~/Documents/orgfiles/**/*",
-            org_default_notes_file = "~/Documents/orgfiles/refile.org",
-        })
+        require("orgmode").setup(opts)
         -- Experimental LSP support
         vim.lsp.enable("org")
     end,
