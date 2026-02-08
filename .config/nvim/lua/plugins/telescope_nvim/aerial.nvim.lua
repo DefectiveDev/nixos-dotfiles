1abc148f0284846dd2d573a10d5096faa3ca36f0
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-07 21:12:33 -0500

    nvim doc(aerial) Aerial has a bug of not loading lsp by default and will load treesitter symbols first.

--- .config/nvim/lua/plugins/telescope_nvim/aerial.nvim.lua
+++ .config/nvim/lua/plugins/telescope_nvim/aerial.nvim.lua
@@ -1,14 +1,12 @@
 return {
     "https://github.com/stevearc/aerial.nvim.git",
     pin = true,
+    -- BUG: Aerial does not load lsp backend first attempt. Other solutions will break due to a lack of a backend.
     keys = {{"<leader>gs", "<cmd>Telescope aerial<cr>", desc = "[g]o to document [s]ymbol in buffer (Aerial + Telescope)" }},
     opts = {
         filter_kind = false,
         backends = { "lsp","treesitter", "markdown", "asciidoc", "man" },
     },
-    config = function (_, opts)
-        require("aerial").setup(opts)
-    end,
     specs = {
         "https://github.com/nvim-telescope/telescope.nvim.git",
         config = function ()
