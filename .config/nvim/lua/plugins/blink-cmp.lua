9519432566629af5b85c5bd212d457e50dee29fb
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-08 11:55:37 -0500

    nvim refactor(blink-cmp) disabled lsp attach loading plugin. Unnecessary.

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -4,7 +4,7 @@ return {
     -- optional: provides snippets for the snippet source
     dependencies = { "https://github.com/rafamadriz/friendly-snippets.git", },
 
-    event = {  "LspAttach", "InsertEnter", "CmdlineEnter" },
+    event = { "InsertEnter", "CmdlineEnter" },
     -- use a release tag to download pre-built binaries
     version = '1.*',
     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
