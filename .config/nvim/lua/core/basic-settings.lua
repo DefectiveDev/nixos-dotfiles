b3aabd5cb70bcb0ee49266af057b53b50a40f471
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-08 18:58:56 -0500

    nvim feat!(keymap) changed from , to \

--- .config/nvim/lua/core/basic-settings.lua
+++ .config/nvim/lua/core/basic-settings.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt
 -- set <Leader> key to space
 vim.g.mapleader = " "
-vim.g.maplocalleader = ","
+vim.g.maplocalleader = "\\"
 
 --use same bar for all buffers
 opt.laststatus = 3
